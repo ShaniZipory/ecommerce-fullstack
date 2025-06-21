6f46ff9ab6a1c4f1986fcf8483527abcc281c241
shani <shanidaniel97@gmail.com>
2025-06-21 14:12:14 +0300

    feat(entites):created typeorm entites

---
+++ backend/src/entities/user.entity.ts
@@ -0,0 +1,26 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToMany } from 'typeorm';
+import { Order } from './order.entity';
+
+@Entity({ schema: 'shop', name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 100 })
+  name: string;
+
+  @Column({ length: 150, unique: true })
+  email: string;
+
+  @Column({ name: 'password_hash', type: 'text' })
+  passwordHash: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @OneToMany(() => Order, (order) => order.user)
+  orders: Order[];
+}
