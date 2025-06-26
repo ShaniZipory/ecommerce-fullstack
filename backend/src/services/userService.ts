ee44ba0fd1748a2043361931e787426d8d9d35af
shani <shanidaniel97@gmail.com>
2025-06-26 20:51:15 +0300

    feat(userService.ts): implemnted signUpService

---
+++ backend/src/services/userService.ts
@@ -0,0 +1,18 @@
+import bcrypt from 'bcrypt';
+import { User } from '../entities/user.entity';
+import { AppDataSource } from '../data-source';
+
+const userRepository = AppDataSource.getRepository(User);
+
+const signUpService = async (name:string, email:string, password:string)=> {
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = new User();
+    user.name = name;
+    user.email = email;
+    user.passwordHash = passwordHash;
+
+    return await userRepository.save(user);
+}
+
+export {signUpService};
