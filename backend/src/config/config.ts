3eb87b556cfb6ae29ad9ed4b566442521a876c7e
shani <shanidaniel97@gmail.com>
2025-06-27 13:43:13 +0300

    feat(userService.ts): implemnted sign in service

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -5,11 +5,13 @@ dotenv.config();
 interface Config {
   port: number;
   nodeEnv: string;
+  jwtSecret: string;
 }
 
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
   nodeEnv: process.env.NODE_ENV || 'development',
+  jwtSecret: process.env.JWT_SECRET || 'f9acf2de3f4b7888a1aa0b2df75829e2bcf48e7da7eff773dc366210963653f9'
 };
 
 export default config;
--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -1,10 +1,12 @@
 import bcrypt from "bcrypt";
 import { User } from '../entities/user.entity';
 import { AppDataSource } from '../data-source';
+import jwt from "jsonwebtoken";
+import config from '../config/config'
 
 const userRepository = AppDataSource.getRepository(User);
 
-const signUpService = async (name:string, email:string, password:string) => {
+const signUpService = async (name: string, email: string, password: string) => {
     const passwordHash = await bcrypt.hash(password, 10);
 
     const user = new User();
@@ -15,4 +17,28 @@ const signUpService = async (name:string, email:string, password:string) => {
     return await userRepository.save(user);
 }
 
-export {signUpService};
+const signInService = async (email: string, password: string) => {
+    const user = await userRepository.findOne({ where: { email } })
+
+    if (!user) {
+        throw { status: 401, message: "Invalid email or password" }
+    }
+
+    const isMatch = await bcrypt.compare(password, user.passwordHash);
+
+    if (!isMatch) {
+        throw { status: 401, message: "Invalid email or password" }
+    }
+
+    const token = jwt.sign(
+        {userId: user.id},
+        config.jwtSecret,
+        {
+            expiresIn: "1h"
+        }
+    );
+
+    return token;
+}
+
+export { signUpService };
